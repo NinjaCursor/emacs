16badcd88c86c83f363711e50f9580bb1c0fbabc
BuildTools <unconfigured@null.spigotmc.org>
2020-02-12 15:45:03 -0500

    first commit

---
+++ init.el
@@ -0,0 +1,44 @@
+;;prevent emacs from saving to current directory
+(setq backup-directory-alist `(("." . "~/.saves")))
+
+
+;; no startup msg  
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(setq inhibit-startup-message t)
+(setq default-directory "c:/Users/jacob/Documents")
+(cd "c:/Users/jacob/Documents")
+
+;;enable packages to be installed from melpa
+(require 'package)
+;; Any add to list for package-archives (to add marmalade or melpa) goes here
+(add-to-list 'package-archives 
+    '("MELPA" .
+      "http://melpa.milkbox.net/packages/"))
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#ffffff" "#37ffff" "#e074e3" "#3732ff" "#ffff0b" "#37ff3c" "#ff400b" "#848088"])
+ '(custom-enabled-themes (quote (dracula)))
+ '(custom-safe-themes
+   (quote
+    ("5f1bd7f67dc1598977e69c6a0aed3c926f49581fdf395a6246f9bc1df86cb030" "3f5f69bfa958dcf04066ab2661eb2698252c0e40b8e61104e3162e341cee1eb9" default)))
+ '(hl-sexp-background-color "#33323e")
+ '(package-selected-packages (quote (dracula-theme leuven-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
